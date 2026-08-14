79d46c1c2967ad4ac286c454b6b506e42e72648f
jorgenuanzs <172059788+jorgenuanzs@users.noreply.github.com>
2026-08-14 14:46:27 +0000

    pact: add 0.10.1 bottle.

--- Formula/pact.rb
+++ Formula/pact.rb
@@ -11,6 +11,12 @@ class Pact < Formula
     strategy :github_latest
   end
 
+  bottle do
+    root_url "https://github.com/jorgenuanzs/homebrew-pact/releases/download/pact-0.10.1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "6ffe0924967d6dda9f48e7c0384c2c8dad87c5ec949dfa4f0989ac0489407a65"
+    sha256 cellar: :any,                 x86_64_linux: "59a82c57416f82ded15c645a7ad5226c53fc73bbdfa4a96412a117340017dc7e"
+  end
+
   depends_on "go" => :build
 
   def install
